07f036f29bb8a2dafda9b2d68933ed8b49833dd2
prameelamarripelly <51620959+prameelamarripelly@users.noreply.github.com>
2025-05-30 14:48:49 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -8,7 +8,6 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
-        System.out.println("GOOD Evening");
          System.out.println("WELCOME TO JENKINS");
          System.out.println("GOOD Evening");
          System.out.println("GOOD Evening");
